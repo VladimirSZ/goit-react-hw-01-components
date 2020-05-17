e22a4611ab1175a9d1913ed0b1ad89c39a6bba60
VladimirSZ <zyryanov.vladimir.s@gmail.com>
2020-05-17 23:36:24 +0300

    Updates

---
+++ precache-manifest.9ee88e1298012174abfafe03ddac51a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34fbed1a04370929caeafd2a871e70b",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "a7c359afcb006f401e25",
+    "url": "/goit-react-hw-01-components/static/css/main.d57a9939.chunk.css"
+  },
+  {
+    "revision": "496511d7c0e906367873",
+    "url": "/goit-react-hw-01-components/static/js/2.bf7b80ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.bf7b80ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7c359afcb006f401e25",
+    "url": "/goit-react-hw-01-components/static/js/main.3a7487ba.chunk.js"
+  },
+  {
+    "revision": "e6e52a6c8a65f3c70b2a",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.69b22fd4.js"
+  }
+]);
